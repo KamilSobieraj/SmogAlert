7e7e35b8484a0e83e5a21ae06078ff3d8672dd8b
Kamil Sobieraj <kamil.sobieraj.dev@gmail.com>
2019-05-21 11:07:53 +0200

    Initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Smog Alert!</h1>
+      </header>
+    </React.Fragment>
+  );
+}
+
+export default App;
